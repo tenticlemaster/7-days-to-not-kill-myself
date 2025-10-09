1f9c2e9b853e132ea64420486ae078e435a26d49
Amir <58517467+tenticlemaster@users.noreply.github.com>
2025-10-09 12:32:07 -0400

    added freeze frame function to player

--- player/states/player.gd
+++ player/states/player.gd
@@ -5,3 +5,13 @@ extends CharacterBody2D
 @export var speed: float
 @export var acceleration: float
 @export var friction: float
+
+
+func freeze_frame(duration: float = 0.4, time_scale: float = 0.1) -> void:
+	Engine.time_scale = time_scale
+	await get_tree().create_timer(duration * time_scale).timeout
+	Engine.time_scale = 1
+
+
+func _on_health_health_update() -> void:
+	freeze_frame()
